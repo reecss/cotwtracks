a92e14e66a8ec129060b7624b87acececc66ab48
Reece Sellars <rsellars355@gmail.com>
2021-06-27 01:44:39 +0100

    Add MediaSession support

--- src/components/episode.js
+++ src/components/episode.js
@@ -3,10 +3,38 @@ import ReactPlayer from 'react-player/lazy';
 import TrackList from './episode/trackList';
 
 const episodes = {
+    302: {
+        episode_number: 302,
+        url: 'https://www.monstercat.com/podcast/302/COTW302.mp3',
+        image: 'https://www.monstercat.com/podcast/302/cover.jpg',
+        name: 'three oh two',
+        tracks: [
+            { "timestamp": 0, "artist": "Monstercat", "name": "Call of the Wild 302"},
+            { "timestamp": 45, "artist": "Dexter King & Aaron Richards", "name": "Heartless"},
+            { "timestamp": 244, "artist": "Justin OH", "name": "Rearview Mirror" },
+            { "timestamp": 400, "artist": "Sabai", "name": "Million Days (ft. Hoang & Claire Ridgely)" },
+            { "timestamp": 588, "artist": "Throttle", "name": "Heroes (ft. NICOLOSI) [Instinct Spotlight]" },
+            { "timestamp": 800, "artist": "Disero & FTKS", "name": "About Us (ft. Winter)" },
+            { "timestamp": 975, "artist": "Vicetone", "name": "Walk Thru Fire (ft. Merōn)" },
+            { "timestamp": 1166, "artist": "CloudNone & Direct", "name": "Margarita" },
+            { "timestamp": 1380, "artist": "FWLR", "name": "Hot VIP (ft. dyl)" },
+            { "timestamp": 1528, "artist": "Dexter King & Alexis Donn", "name": "Only You" },
+            { "timestamp": 1711, "artist": "Dexter King", "name": "Get To Know You (ft. Aviella)" },
+            { "timestamp": 1891, "artist": "Julian Calor", "name": "Arp of Astronomical Wisdom" },
+            { "timestamp": 2089, "artist": "inverness & Amelia Moore", "name": "Toxic [Monstercat Exclusive]" },
+            { "timestamp": 2234, "artist": "FWLR", "name": "Bust It Out" },
+            { "timestamp": 2422, "artist": "Nonsens & Ericka Jane", "name": "We All" },
+            { "timestamp": 2610, "artist": "Terry Zhong", "name": "Night Cap (ft. Will Jay)" },
+            { "timestamp": 2820, "artist": "Gareth Emery", "name": "Somebody (ft. Kovic)" },
+            { "timestamp": 3023, "artist": "Vicetone", "name": "I Feel Human (ft. BullySongs)" },
+            { "timestamp": 3199, "artist": "Bad Computer", "name": "2U" },
+            { "timestamp": 3391, "artist": "Stonebank & EMEL", "name": "Time [Uncaged Spotlight]" },
+        ]
+    },
     303: {
         episode_number: 303,
         url: 'https://www.monstercat.com/podcast/303/COTW303.mp3',
-        image: 'http://www.monstercat.com/podcast/303/cover.jpg',
+        image: 'https://www.monstercat.com/podcast/303/cover.jpg',
         name: 'three oh three',
         tracks: [
 			{ "timestamp": 45, "artist": "Aero Chord", "name": "The 90s" },
@@ -31,6 +59,10 @@ const episodes = {
     }
 }
 
+const TRACK_TYPE_CURRENT = 'current';
+const TRACK_TYPE_NEXT = 'next';
+const TRACK_TYPE_PREV = 'prev';
+
 class Episode extends Component {
     state = {
         pip: false,
@@ -48,21 +80,41 @@ class Episode extends Component {
         currentTrack: null,
     }
 
+    componentDidMount() {
+        if ("mediaSession" in navigator) {
+            window.navigator.mediaSession.setActionHandler('previoustrack', () => {
+                const currentTime = this.player.getCurrentTime();
+
+                // we're more than 3 seconds into the track, so skip to the start of the current song
+                if (currentTime >= this.state.currentTrack.timestamp + 3) {
+                    this.seekToTrack(this.state.currentTrack);
+                }
+                // if we're less than 3 seconds in, skip back to the previous song
+                else {
+                    let prevTrack = this.getPrevTrack();
+
+                    if (typeof prevTrack !== 'undefined') {
+                        this.seekToTrack(prevTrack);
+                    }
+                }
+            });
+
+            window.navigator.mediaSession.setActionHandler('nexttrack', () => {
+                let nextTrack = this.getNextTrack();
+
+                if (typeof nextTrack !== 'undefined') {
+                    this.seekToTrack(nextTrack);
+                }
+            });
+        }
+    }
+
+    handleSeek = () => {
+        this.updateCurrentTrack();
+    }
+
     seekToTrack = track => {
         this.player.seekTo(track.timestamp);
-    }
-
-    handleSeekMouseDown = e => {
-        this.setState({ seeking: true })
-    }
-
-    handleSeekChange = e => {
-        this.setState({ played: parseFloat(e.target.value) })
-    }
-
-    handleSeekMouseUp = e => {
-        this.setState({ seeking: false })
-        this.player.seekTo(parseFloat(e.target.value))
     }
 
     handleProgress = state => {
@@ -72,6 +124,33 @@ class Episode extends Component {
         }
 
         this.updateCurrentTrack();
+    }
+
+    getCurrentTrack = () => {
+        return this.getTrack(TRACK_TYPE_CURRENT);
+    }
+
+    getPrevTrack = () => {
+        return this.getTrack(TRACK_TYPE_PREV);
+    }
+
+    getNextTrack = () => {
+        return this.getTrack(TRACK_TYPE_NEXT);
+    }
+
+    getTrack = (type) => {
+        const currentTrackIndex = this.state.episode.tracks.indexOf(this.state.currentTrack)
+
+        switch (type) {
+            case TRACK_TYPE_CURRENT:
+                return this.state.currentTrack;
+            case TRACK_TYPE_PREV:
+                return this.state.episode.tracks[currentTrackIndex-1] || undefined;
+            case TRACK_TYPE_NEXT:
+                return this.state.episode.tracks[currentTrackIndex+1] || undefined;
+            default:
+                return undefined;
+        }
     }
 
     updateCurrentTrack = () => {
@@ -92,7 +171,21 @@ class Episode extends Component {
             }
         }, this);
 
-        this.setState({ currentTrack: currentTrack });
+        if (typeof currentTrack !== 'undefined' && currentTrack !== this.state.currentTrack) {
+            this.setState({ currentTrack: currentTrack });
+
+            if ("mediaSession" in navigator) {
+                window.navigator.mediaSession.metadata = new window.MediaMetadata({
+                    title: this.state.currentTrack.name,
+                    artist: this.state.currentTrack.artist,
+                    album: this.state.episode.name,
+                    artwork: [
+                        {src: this.state.episode.image, sizes: '256x256', type: 'image/jpeg'},
+                        {src: this.state.episode.image, sizes: '512x512', type: 'image/jpeg'}
+                    ]
+                });
+            }
+        }
     }
 
     ref = player => {
@@ -115,14 +208,10 @@ class Episode extends Component {
                             forceAudio: true,
                         }
                     }}
+                    onSeek={this.handleSeek}
                     onProgress={this.handleProgress}
-                />
-                <input
-                    type='range' min={0} max={0.999999} step='any'
-                    value={this.state.played}
-                    onMouseDown={this.handleSeekMouseDown}
-                    onChange={this.handleSeekChange}
-                    onMouseUp={this.handleSeekMouseUp}
+                    onPlay={this.handlePlay}
+                    onPause={this.handlePause}
                 />
             </React.Fragment>
           );
